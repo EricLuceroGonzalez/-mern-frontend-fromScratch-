45e720158a5c701475c72592e8588431d946cd80
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-02-11 21:41:06 -0500

    First reacstrap element and First FUNCTION

--- src/Componentes/Counter.js
+++ src/Componentes/Counter.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import { Button } from "reactstrap";
 
 class Counter extends Component {
   state = { valor: this.props.value };
+
+  //The first function:
+  incrementCounter(){
+      console.log('Im here!');
+      
+  }
   render() {
     return (
       <React.Fragment>
         <div style={{ backgroundColor: "rgba(202,202,202,0.75)" }}>
           <h3>{this.props.title}</h3>
           <div>{this.state.valor}</div>
+          <Button onClick={this.incrementCounter()}> + </Button>
         </div>
       </React.Fragment>
     );
