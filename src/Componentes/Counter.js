d51f50066c8c50475168076cee8ed3c98fd7978f
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-02-11 22:14:01 -0500

    Button + increment function to modify states

--- src/Componentes/Counter.js
+++ src/Componentes/Counter.js
@@ -5,9 +5,9 @@ class Counter extends Component {
   state = { valor: this.props.value };
 
   //The first function:
-  incrementCounter(){
-      console.log('Im here!');
-      
+  incrementCounter =() => {
+    console.log("Im here!");
+    this.setState({ valor: this.state.valor + 1 });
   }
   render() {
     return (
@@ -15,7 +15,7 @@ class Counter extends Component {
         <div style={{ backgroundColor: "rgba(202,202,202,0.75)" }}>
           <h3>{this.props.title}</h3>
           <div>{this.state.valor}</div>
-          <Button onClick={this.incrementCounter()}> + </Button>
+          <Button onClick={this.incrementCounter}> + </Button>
         </div>
       </React.Fragment>
     );
