86b8ed167821b9e36cf4659d507d30b542149814
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-02-12 20:57:25 -0500

    Creation of counters as state and instantiate by render in a map() function

--- src/Componentes/Counters.js
+++ src/Componentes/Counters.js
@@ -6,14 +6,29 @@ const counterSty = {
   padding: "18px 12px"
 };
 class Counters extends Component {
-  state = {};
+  state = {
+    contadores: [
+      { id: 0, title: "Alpha", value: 0 },
+      { id: 1, title: "Beta", value: 5 },
+      { id: 2, title: "Gamma", value: -10 },
+      { id: 3, title: "Omega", value: 0 },
+      { id: 4, title: "Omega-2", value: 3 }
+    ]
+  };
+
+  renderCounters() {
+    const allCounters = this.state.contadores.map((item, iKey) => {
+      return (
+        <Counter key={item.id} title={item.title} value={item.value}></Counter>
+      );
+    });
+    return allCounters;
+  }
   render() {
     return (
       <div style={counterSty}>
         <h2>Counters Component</h2>
-        <Counter title={"The props Title"} value={4}></Counter>
-        <Counter title={"Another counter component"} value={-4}></Counter>
-        <Counter title={"and another"} value={0}></Counter>
+        {this.renderCounters()}
       </div>
     );
   }
